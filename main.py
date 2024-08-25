0d7ff8d1526878d964bc0394f98afd8ca721829f
alondravi <alondravillagrana@hotmail.com>
2024-08-25 13:10:11 -0600

    main.py

--- main.py
+++ main.py
@@ -1,57 +1,6 @@
 import data
 from selenium import webdriver
-from selenium.webdriver import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-
-
-# no modificar
-def retrieve_phone_code(driver) -> str:
-    """Este código devuelve un número de confirmación de teléfono y lo devuelve como un string.
-    Utilízalo cuando la aplicación espere el código de confirmación para pasarlo a tus pruebas.
-    El código de confirmación del teléfono solo se puede obtener después de haberlo solicitado en la aplicación."""
-
-    import json
-    import time
-    from selenium.common import WebDriverException
-    code = None
-    for i in range(10):
-        try:
-            logs = [log["message"] for log in driver.get_log('performance') if log.get("message")
-                    and 'api/v1/number?number' in log.get("message")]
-            for log in reversed(logs):
-                message_data = json.loads(log)["message"]
-                body = driver.execute_cdp_cmd('Network.getResponseBody',
-                                              {'requestId': message_data["params"]["requestId"]})
-                code = ''.join([x for x in body['body'] if x.isdigit()])
-        except WebDriverException:
-            time.sleep(1)
-            continue
-        if not code:
-            raise Exception("No se encontró el código de confirmación del teléfono.\n"
-                            "Utiliza 'retrieve_phone_code' solo después de haber solicitado el código en tu aplicación.")
-        return code
-
-
-class UrbanRoutesPage:
-    from_field = (By.ID, 'from')
-    to_field = (By.ID, 'to')
-
-    def __init__(self, driver):
-        self.driver = driver
-
-    def set_from(self, from_address):
-        self.driver.find_element(*self.from_field).send_keys(from_address)
-
-    def set_to(self, to_address):
-        self.driver.find_element(*self.to_field).send_keys(to_address)
-
-    def get_from(self):
-        return self.driver.find_element(*self.from_field).get_property('value')
-
-    def get_to(self):
-        return self.driver.find_element(*self.to_field).get_property('value')
+from methods import UrbanRoutesPage
 
 
 
@@ -65,7 +14,7 @@ class TestUrbanRoutes:
         from selenium.webdriver import DesiredCapabilities
         capabilities = DesiredCapabilities.CHROME
         capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
-        cls.driver = webdriver.Chrome(desired_capabilities=capabilities)
+        cls.driver = webdriver.Chrome()
 
     def test_set_route(self):
         self.driver.get(data.urban_routes_url)
@@ -76,6 +25,40 @@ class TestUrbanRoutes:
         assert routes_page.get_from() == address_from
         assert routes_page.get_to() == address_to
 
+    def test_select_comfort_tariff(self):
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.select_comfort_tariff()
+        assert routes_page.verify_comfort_tariff_is_selected() is True
+
+    def test_phone_number_verification(self):
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.complete_phone_verification()
+        assert routes_page.get_phone_number() == data.phone_number
+
+    def test_add_payment_card(self):
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.add_payment_card(data.card_number, data.card_code)
+        assert routes_page.verify_payment_method_added() is True
+
+    def test_write_message_for_driver(self):
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.write_message_for_driver(data.message_for_driver)
+        assert routes_page.get_message_for_driver() == data.message_for_driver
+
+    def test_select_blanket_and_tissues(self):
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.select_blanket_and_tissues()
+        assert routes_page.check_blanket_and_tissues_is_selected() is True
+
+    def test_ice_cream_order(self):
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.add_two_ice_creams()
+        assert routes_page.verify_ice_cream_order()
+
+    def test_book_a_taxi(self):
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.click_on_book_a_taxi()
+        assert routes_page.get_taxi_search_status() is True
 
     @classmethod
     def teardown_class(cls):
